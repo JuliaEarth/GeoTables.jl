92a46e50662ed52bdec553f16811e258b35f0ef9
Erick <eral.th07@gmail.com>
2023-05-02 20:10:21 -0500

    test for reading files from disk

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using GeoTables
 using Tables
 using Meshes
 using Test, Random
+using Dates
 import Shapefile as SHP
 import ArchGDAL as AG
 import GeoJSON as GJ
@@ -67,32 +68,139 @@ datadir = joinpath(@__DIR__,"data")
   end
 
   @testset "load" begin
-    table = GeoTables.load(joinpath(datadir,"path.shp"))
-    @test Tables.schema(table).names == (:ZONA, :geometry)
-    @test nitems(table) == 6
-    @test table.ZONA == ["PA 150","BR 364","BR 163","BR 230","BR 010","Estuarina PA"]
-    @test table.geometry isa GeometrySet
-    @test table[1,:geometry] isa Multi
 
-    table = GeoTables.load(joinpath(datadir,"zone.shp"))
-    @test Tables.schema(table).names == (:PERIMETER, :ACRES, :MACROZONA, :Hectares, :area_m2, :geometry)
-    @test nitems(table) == 4
-    @test table.PERIMETER == [5.850803650776888e6, 9.539471535859613e6, 1.01743436941e7, 7.096124186552936e6]
-    @test table.ACRES == [3.23144676827e7, 2.50593712407e8, 2.75528426573e8, 1.61293042687e8]
-    @test table.MACROZONA == ["Estuario","Fronteiras Antigas","Fronteiras Intermediarias","Fronteiras Novas"]
-    @test table.Hectares == [1.30772011078e7, 1.01411677447e8, 1.11502398263e8, 6.52729785685e7]
-    @test table.area_m2 == [1.30772011078e11, 1.01411677447e12, 1.11502398263e12, 6.52729785685e11]
-    @test table.geometry isa GeometrySet
-    @test table[1,:geometry] isa Multi
+    @testset "custom datasets" begin
+      table = GeoTables.load(joinpath(datadir,"path.shp"))
+      @test Tables.schema(table).names == (:ZONA, :geometry)
+      @test nitems(table) == 6
+      @test table.ZONA == ["PA 150","BR 364","BR 163","BR 230","BR 010","Estuarina PA"]
+      @test table.geometry isa GeometrySet
+      @test table[1,:geometry] isa Multi
 
-    table = GeoTables.load(joinpath(datadir,"ne_110m_land.shp"))
-    @test Tables.schema(table).names == (:featurecla, :scalerank, :min_zoom, :geometry)
-    @test nitems(table) == 127
-    @test all(==("Land"), table.featurecla)
-    @test all(∈([0,1]), table.scalerank)
-    @test all(∈([0.0,0.5,1.0,1.5]), table.min_zoom)
-    @test table.geometry isa GeometrySet
-    @test table[1,:geometry] isa Multi
+      table = GeoTables.load(joinpath(datadir,"zone.shp"))
+      @test Tables.schema(table).names == (:PERIMETER, :ACRES, :MACROZONA, :Hectares, :area_m2, :geometry)
+      @test nitems(table) == 4
+      @test table.PERIMETER == [5.850803650776888e6, 9.539471535859613e6, 1.01743436941e7, 7.096124186552936e6]
+      @test table.ACRES == [3.23144676827e7, 2.50593712407e8, 2.75528426573e8, 1.61293042687e8]
+      @test table.MACROZONA == ["Estuario","Fronteiras Antigas","Fronteiras Intermediarias","Fronteiras Novas"]
+      @test table.Hectares == [1.30772011078e7, 1.01411677447e8, 1.11502398263e8, 6.52729785685e7]
+      @test table.area_m2 == [1.30772011078e11, 1.01411677447e12, 1.11502398263e12, 6.52729785685e11]
+      @test table.geometry isa GeometrySet
+      @test table[1,:geometry] isa Multi
+
+      table = GeoTables.load(joinpath(datadir,"ne_110m_land.shp"))
+      @test Tables.schema(table).names == (:featurecla, :scalerank, :min_zoom, :geometry)
+      @test nitems(table) == 127
+      @test all(==("Land"), table.featurecla)
+      @test all(∈([0,1]), table.scalerank)
+      @test all(∈([0.0,0.5,1.0,1.5]), table.min_zoom)
+      @test table.geometry isa GeometrySet
+      @test table[1,:geometry] isa Multi
+    end
+
+    @testset "Shapefile" begin
+      varnames = (:code, :name, :date, :variable, :geometry)
+
+      table = GeoTables.load(joinpath(datadir, "points.shp"))
+      @test Tables.schema(table).names == varnames
+      @test nitems(table) == 5
+      @test table.code[1] isa Integer
+      @test table.name[1] isa String
+      @test table.date[1] isa Date
+      @test table.variable[1] isa Real
+      @test table.geometry isa PointSet
+      @test table[1,:geometry] isa Point
+
+      table = GeoTables.load(joinpath(datadir, "lines.shp"))
+      @test Tables.schema(table).names == varnames
+      @test nitems(table) == 5
+      @test table.code[1] isa Integer
+      @test table.name[1] isa String
+      @test table.date[1] isa Date
+      @test table.variable[1] isa Real
+      @test table.geometry isa GeometrySet
+      @test table[1, :geometry] isa Multi
+      @test collect(table[1, :geometry])[1] isa Chain
+
+      table = GeoTables.load(joinpath(datadir, "polygons.shp"))
+      @test Tables.schema(table).names == varnames
+      @test nitems(table) == 5
+      @test table.code[1] isa Integer
+      @test table.name[1] isa String
+      @test table.date[1] isa Date
+      @test table.variable[1] isa Real
+      @test table.geometry isa GeometrySet
+      @test table[1, :geometry] isa Multi
+      @test collect(table[1, :geometry])[1] isa PolyArea
+    end
+
+    @testset "GeoJSON" begin
+      varnames = (:code, :name, :date, :variable, :geometry)
+
+      table = GeoTables.load(joinpath(datadir, "points.geojson"))
+      @test isempty(setdiff(Tables.schema(table).names, varnames))
+      @test nitems(table) == 5
+      @test table.code[1] isa Integer
+      @test table.name[1] isa String
+      @test table.date[1] isa String
+      @test table.variable[1] isa Real
+      @test table.geometry isa PointSet
+      @test table[1,:geometry] isa Point
+
+      table = GeoTables.load(joinpath(datadir, "lines.geojson"))
+      @test isempty(setdiff(Tables.schema(table).names, varnames))
+      @test nitems(table) == 5
+      @test table.code[1] isa Integer
+      @test table.name[1] isa String
+      @test table.date[1] isa String
+      @test table.variable[1] isa Real
+      @test table.geometry isa GeometrySet
+      @test table[1, :geometry] isa Chain
+
+      table = GeoTables.load(joinpath(datadir, "polygons.geojson"))
+      @test isempty(setdiff(Tables.schema(table).names, varnames))
+      @test nitems(table) == 5
+      @test table.code[1] isa Integer
+      @test table.name[1] isa String
+      @test table.date[1] isa String
+      @test table.variable[1] isa Real
+      @test table.geometry isa GeometrySet
+      @test table[1, :geometry] isa PolyArea
+    end
+
+    @testset "ArchGDAL" begin
+      varnames = (:code, :name, :date, :variable, :geometry)
+
+      table = GeoTables.load(joinpath(datadir, "points.gpkg"))
+      @test Tables.schema(table).names == varnames
+      @test nitems(table) == 5
+      @test table.code[1] isa Integer
+      @test table.name[1] isa String
+      @test table.date[1] isa DateTime
+      @test table.variable[1] isa Real
+      @test table.geometry isa PointSet
+      @test table[1,:geometry] isa Point
+
+      table = GeoTables.load(joinpath(datadir, "lines.gpkg"))
+      @test Tables.schema(table).names == varnames
+      @test nitems(table) == 5
+      @test table.code[1] isa Integer
+      @test table.name[1] isa String
+      @test table.date[1] isa DateTime
+      @test table.variable[1] isa Real
+      @test table.geometry isa GeometrySet
+      @test table[1, :geometry] isa Chain
+
+      table = GeoTables.load(joinpath(datadir, "polygons.gpkg"))
+      @test Tables.schema(table).names == varnames
+      @test nitems(table) == 5
+      @test table.code[1] isa Integer
+      @test table.name[1] isa String
+      @test table.date[1] isa DateTime
+      @test table.variable[1] isa Real
+      @test table.geometry isa GeometrySet
+      @test table[1, :geometry] isa PolyArea
+    end
   end
 
   @testset "save" begin
