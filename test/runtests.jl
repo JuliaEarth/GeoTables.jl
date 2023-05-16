42b1030b98eacb3e9d962c7377277e5f1c3736a3
Elias Carvalho <eliascarvdev@gmail.com>
2023-05-16 16:34:04 -0300

    Use smaller countries

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,11 +14,6 @@ islinux = Sys.islinux()
 datadir = joinpath(@__DIR__,"data")
 savedir = mktempdir()
 
-# download files
-GeoTables.gadm("USA")
-GeoTables.gadm("BRA")
-GeoTables.gadm("IND")
-
 @testset "GeoTables.jl" begin
   @testset "convert" begin
     points = Point2[(0,0),(0.5,2),(2.2,2.2)]
@@ -273,13 +268,13 @@ GeoTables.gadm("IND")
   end
 
   @testset "gadm" begin
-    table = GeoTables.gadm("BRA", depth = 1, ϵ=0.04)
-    @test nitems(table) == 27
+    table = GeoTables.gadm("SVN", depth=1, ϵ=0.04)
+    @test nitems(table) == 12
 
-    table = GeoTables.gadm("USA", depth = 1, ϵ=0.04)
-    @test nitems(table) == 51
+    table = GeoTables.gadm("QAT", depth=1, ϵ=0.04)
+    @test nitems(table) == 7
 
-    table = GeoTables.gadm("IND", depth = 1)
-    @test nitems(table) == 36
+    table = GeoTables.gadm("ISR", depth=1)
+    @test nitems(table) == 7
   end
 end
