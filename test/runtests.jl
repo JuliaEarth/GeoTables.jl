f25345726a16b2519f459d264bb87355a23857d4
Elias Carvalho <eliascarvdev@gmail.com>
2023-09-01 13:22:11 -0300

    Remove unused dep

--- test/runtests.jl
+++ test/runtests.jl
@@ -187,26 +187,4 @@ dummymeta(domain, table) = GeoTable(domain, Dict(paramdim(domain) => table))
       @test v.b == v."b" == [6, 7, 8]
     end
   end
-
-  @testset "partitioning" begin
-    data = geotable(CartesianGrid(10, 10), etable=(a=rand(100), b=rand(100)))
-    for method in [
-      UniformPartition(2),
-      FractionPartition(0.5),
-      BlockPartition(2),
-      BallPartition(2),
-      BisectPointPartition(Vec(1, 1), Point(5, 5)),
-      BisectFractionPartition(Vec(1, 1), 0.5),
-      PlanePartition(Vec(1, 1)),
-      DirectionPartition(Vec(1, 1)),
-      PredicatePartition((i, j) -> iseven(i + j)),
-      SpatialPredicatePartition((x, y) -> norm(x + y) < 5),
-      ProductPartition(UniformPartition(2), UniformPartition(2)),
-      HierarchicalPartition(UniformPartition(2), UniformPartition(2))
-    ]
-      Π = partition(data, method)
-      inds = reduce(vcat, indices(Π))
-      @test sort(inds) == 1:100
-    end
-  end
 end
