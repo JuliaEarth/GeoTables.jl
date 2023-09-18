d1368a9474ebdcefd4b09653f35b9d1459d7f0ba
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-09-18 10:32:16 -0300

    Add missing header

--- src/subgeotable.jl
+++ src/subgeotable.jl
@@ -1,3 +1,7 @@
+# ------------------------------------------------------------------
+# Licensed under the MIT License. See LICENSE in the project root.
+# ------------------------------------------------------------------
+
 """
     SubGeoTable(geotable, inds)
 
