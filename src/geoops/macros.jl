3d735f1ee5c7e43cb3da204e2db9c31c05f084a9
Elias Carvalho <73039601+eliascarv@users.noreply.github.com>
2023-12-11 17:04:35 -0300

    Refactor the code generation of the geoops macros (#87)
    * Refactor the code generation of the geoops macros
    
    * Fix typo
    
    * Fix typo
    
    * Add header
    
    * Update code

--- src/geoops/macros.jl
+++ src/geoops/macros.jl
@@ -2,11 +2,9 @@
 # Licensed under the MIT License. See LICENSE in the project root.
 # ------------------------------------------------------------------
 
-_expr_error() = throw(ArgumentError("invalid expression"))
-
 function _split(expr::Expr, rowwise=true)
   if expr.head ≠ :(=)
-    _expr_error()
+    throw(ArgumentError("invalid expression syntax"))
   end
 
   colname = _colname(expr.args[1])
@@ -15,60 +13,98 @@ function _split(expr::Expr, rowwise=true)
   colname, colexpr
 end
 
-function _colname(expr::Expr)
-  if expr.head == :braces
-    :(Symbol($(esc(expr.args[1]))))
+# returns a column name expression from the LHS expression
+function _colname(expr)
+  if _iscolname(expr)
+    _nameexpr(expr)
   else
-    _expr_error()
+    throw(ArgumentError("invalid column name syntax"))
   end
 end
 
-_colname(nm::QuoteNode) = nm
-_colname(::Any) = _expr_error()
-
-function _colexpr(expr::Expr, rowwise)
-  if expr.head == :braces
-    _makeexpr(expr)
+# returns a column generator expression from the RHS expression
+function _colexpr(expr, rowwise)
+  if _iscolname(expr)
+    # if the RHS expression is a column name expression
+    # returns a get column expression
+    _getcolexpr(expr)
   else
-    colexpr = copy(expr)
-    _preprocess!(colexpr, rowwise)
-    colexpr
-  end
-end
-
-_colexpr(nm::QuoteNode, _) = _makeexpr(nm)
-_colexpr(var::Symbol, _) = esc(var)
-_colexpr(::Any, _) = _expr_error()
-
-_makeexpr(expr::Expr) = :(getproperty(geotable, $(esc(expr.args[1]))))
-_makeexpr(nm::QuoteNode) = :(getproperty(geotable, $nm))
-
-function _preprocess!(expr::Expr, rowwise)
-  if expr.head ≠ :call
-    _expr_error()
-  end
-
-  if rowwise
-    pushfirst!(expr.args, :broadcast)
-  end
-
-  for (i, arg) in enumerate(expr.args)
-    if arg isa Symbol
-      expr.args[i] = esc(arg)
-    end
-
-    if arg isa QuoteNode
-      expr.args[i] = _makeexpr(arg)
-    end
-
-    if arg isa Expr
-      if arg.head == :(.)
-        expr.args[i] = esc(arg)
-      elseif arg.head == :braces
-        expr.args[i] = _makeexpr(arg)
+    colnames = _colnames(expr)
+    if isempty(colnames)
+      # if the RHS expression doesn't have column name expressions
+      # returns a constant column expression
+      constexpr = esc(expr)
+      if rowwise
+        :(fill($constexpr, nrow(geotable)))
       else
-        _preprocess!(arg, rowwise)
+        constexpr
       end
+    else
+      # otherwise, returns a function expression
+      funexpr = _funexpr(expr, colnames)
+      columns = map(_getcolexpr, colnames)
+      if rowwise
+        :(map($funexpr, $(columns...)))
+      else
+        :($funexpr($(columns...)))
+      end
+    end
+  end
+end
+
+# checks if expression is a column name expression
+_iscolname(expr) = expr isa QuoteNode || Meta.isexpr(expr, :braces)
+
+# creates the executable version of the column name expression 
+_nameexpr(expr::QuoteNode) = expr
+_nameexpr(expr::Expr) = :(Symbol($(esc(expr.args[1]))))
+
+# creates the get column expression
+_getcolexpr(expr) = :(getproperty(geotable, $(_nameexpr(expr))))
+
+# collects all column name expressions from the RHS expression
+function _colnames(expr)
+  colnames = []
+  if expr isa Expr
+    _colargs!(expr, colnames)
+  end
+  colnames
+end
+
+function _colargs!(expr, colnames)
+  start = Meta.isexpr(expr, [:call, :macrocall]) ? 2 : 1
+  for i in start:length(expr.args)
+    arg = expr.args[i]
+    if _iscolname(arg)
+      push!(colnames, arg)
+    elseif arg isa Expr
+      _colargs!(arg, colnames)
+    end
+  end
+end
+
+# create the function expression from the RHS expression
+function _funexpr(expr, colnames)
+  funargs = [gensym() for _ in colnames]
+  argsym = Dict(zip(colnames, funargs))
+  funbody = _funbody(expr, argsym)
+  :(($(funargs...),) -> $funbody)
+end
+
+function _funbody(expr, argsym)
+  funbody = copy(expr)
+  _funbody!(funbody, argsym)
+  funbody
+end
+
+function _funbody!(expr, argsym)
+  for (i, arg) in enumerate(expr.args)
+    if haskey(argsym, arg)
+      expr.args[i] = argsym[arg]
+    elseif arg isa Symbol
+      expr.args[i] = esc(arg)
+    elseif arg isa Expr
+      _funbody!(arg, argsym)
     end
   end
 end
