ce175b8e490fd7705bcb819d564f378bc9633eb9
Elias Carvalho <73039601+eliascarv@users.noreply.github.com>
2023-12-14 13:39:58 -0300

    Update extension to latest version of TableTransforms.jl (#88)

--- ext/GeoTablesTableTransformsExt.jl
+++ ext/GeoTablesTableTransformsExt.jl
@@ -28,91 +28,40 @@ attach(table, dom::Domain) = georef(table, dom)
 
 function applymeta(::Sort, dom::Domain, prep)
   sinds = prep
-
   sdom = view(dom, sinds)
-
-  sdom, sinds
-end
-
-function revertmeta(::Sort, newdom::Domain, mcache)
-  sinds = mcache
-  rinds = sortperm(sinds)
-
-  view(newdom, rinds)
+  sdom, nothing
 end
 
 # --------------------------------------------------
 
 function applymeta(::Filter, dom::Domain, prep)
-  sinds, rinds = prep
-
+  sinds = prep
   sdom = view(dom, sinds)
-  rdom = view(dom, rinds)
-
-  sdom, (rinds, rdom)
-end
-
-function revertmeta(::Filter, newdom::Domain, mcache)
-  geoms = collect(newdom)
-
-  rinds, rdom = mcache
-  for (i, geom) in zip(rinds, rdom)
-    insert!(geoms, i, geom)
-  end
-
-  GeometrySet(geoms)
+  sdom, nothing
 end
 
 # --------------------------------------------------
 
 function applymeta(::DropMissing, dom::Domain, prep)
   ftrans, fprep, _ = prep
-  newmeta, fmcache = applymeta(ftrans, dom, fprep)
-  newmeta, (ftrans, fmcache)
-end
-
-function revertmeta(::DropMissing, newdom::Domain, mcache)
-  ftrans, fmcache = mcache
-  revertmeta(ftrans, newdom, fmcache)
+  newmeta, _ = applymeta(ftrans, dom, fprep)
+  newmeta, nothing
 end
 
 # --------------------------------------------------
 
 function applymeta(::DropExtrema, dom::Domain, prep)
   ftrans, fprep = prep
-  newmeta, fmcache = applymeta(ftrans, dom, fprep)
-  newmeta, (ftrans, fmcache)
-end
-
-function revertmeta(::DropExtrema, newdom::Domain, mcache)
-  ftrans, fmcache = mcache
-  revertmeta(ftrans, newdom, fmcache)
+  newmeta, _ = applymeta(ftrans, dom, fprep)
+  newmeta, nothing
 end
 
 # --------------------------------------------------
 
 function applymeta(::Sample, dom::Domain, prep)
-  sinds, rinds = prep
-
+  sinds = prep
   sdom = view(dom, sinds)
-  rdom = view(dom, rinds)
-
-  sdom, (sinds, rinds, rdom)
-end
-
-function revertmeta(::Sample, newdom::Domain, mcache)
-  geoms = collect(newdom)
-
-  sinds, rinds, rdom = mcache
-
-  uinds = indexin(sort(unique(sinds)), sinds)
-  ugeoms = [geoms[i] for i in uinds]
-
-  for (i, geom) in zip(rinds, rdom)
-    insert!(ugeoms, i, geom)
-  end
-
-  GeometrySet(ugeoms)
+  sdom, nothing
 end
 
 # ---------------------
