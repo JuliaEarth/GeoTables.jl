2cb5d4fd54ff2470e7781f4b98de71680e74f804
Elias Carvalho <eliascarvdev@gmail.com>
2024-02-21 09:42:49 -0300

    Add support for row indexing with a vector of bools

--- src/api/dataframes.jl
+++ src/api/dataframes.jl
@@ -38,13 +38,16 @@ end
 
 Base.getproperty(geotable::AbstractGeoTable, var::AbstractString) = getproperty(geotable, Symbol(var))
 
-const RowSelector = Union{Int,AbstractVector{Int},Colon}
+const RowSelector = Union{Int,AbstractVector{Int},AbstractVector{Bool},Colon}
 
 Base.getindex(geotable::AbstractGeoTable, ::Colon, ::Colon) = geotable
 
 Base.getindex(geotable::AbstractGeoTable, inds::AbstractVector{Int}, ::Colon) = view(geotable, inds)
 
 Base.getindex(geotable::AbstractGeoTable, rows::RowSelector, vars) = _getindex(geotable, rows, selector(vars))
+
+_getindex(geotable::AbstractGeoTable, inds::AbstractVector{Bool}, selector::ColumnSelector) =
+  _getindex(geotable, (1:nrow(geotable))[inds], selector)
 
 function _getindex(geotable::AbstractGeoTable, rows::RowSelector, selector::ColumnSelector)
   svars = selector(propertynames(geotable))
