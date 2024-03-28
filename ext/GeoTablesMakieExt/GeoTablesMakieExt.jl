5bc7a2bd41545de3359139cf390d44f8857d74ac
Júlio Hoffimann <julio.hoffimann@gmail.com>
2024-03-28 15:12:31 -0300

    Better error messages for viz and viewer

--- ext/GeoTablesMakieExt/GeoTablesMakieExt.jl
+++ ext/GeoTablesMakieExt/GeoTablesMakieExt.jl
@@ -20,12 +20,34 @@ using DataScienceTraits: Distributional
 using DataScienceTraits: Unknown
 
 import Makie
-import Meshes: ascolors, defaultscheme
+import Meshes: viz, viz!
+import Meshes: ascolors
+import Meshes: defaultscheme
 import GeoTables: viewer
 
 const CategArray{T,N} = Union{CategoricalArray{T,N},SubArray{T,N,<:CategoricalArray}}
 
 include("colors.jl")
 include("viewer.jl")
+
+# ----------------
+# COMMON MISTAKES
+# ----------------
+
+viz(::AbstractGeoTable, args...; kwargs...) = vizmistake()
+viz!(::AbstractGeoTable, args...; kwargs...) = vizmistake()
+vizmistake() = throw(ArgumentError(
+  """
+  Expected a `Geometry`, `Domain` or vector of these types. Got a `GeoTable` instead.
+  Use the `viewer` to visualize all the columns of the `GeoTable` over its domain.
+  """
+))
+
+viewer(args...; kwargs...) = viewermistake()
+viewermistake() = throw(ArgumentError(
+  """
+  Expected a `GeoTable`. Use `viz` to visualize a `Geometry`, `Domain` or vector of these types.
+  """
+))
 
 end
