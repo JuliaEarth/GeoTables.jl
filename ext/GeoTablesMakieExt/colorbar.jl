b25356024b13ebf47e8fdf55ac6f67bfacc02235
Elias Carvalho <eliascarvdev@gmail.com>
2024-04-05 11:42:01 -0300

    Support colorrange

--- ext/GeoTablesMakieExt/colorbar.jl
+++ ext/GeoTablesMakieExt/colorbar.jl
@@ -7,26 +7,27 @@ cbar(fig, values; kwargs...) = _cbar(fig, Colorfier(values; kwargs...))
 function _cbar(fig, colorfier)
   values = Colorfy.values(colorfier)
   colorscheme = Colorfy.colorscheme(colorfier)
+  colorrange = Colorfy.colorrange(colorfier)
   colormap = defaultcolormap(values, colorscheme)
-  limits = defaultlimits(values)
-  ticks = defaultticks(values)
+  limits = defaultlimits(values, colorrange)
+  ticks = defaultticks(values, limits)
   tickformat = defaultformat(values)
   Makie.Colorbar(fig; colormap, limits, ticks, tickformat)
 end
 
-defaultcolormap(_, colorscheme) = colorscheme
-function defaultcolormap(values::CategArray, colorscheme)
-  nlevels = length(levels(values))
+defaultcolormap(vals, colorscheme) = colorscheme
+function defaultcolormap(vals::CategArray, colorscheme)
+  nlevels = length(levels(vals))
   Makie.cgrad(colorscheme[1:nlevels], nlevels, categorical=true)
 end
 
-defaultlimits(vals) = defaultlimits(elscitype(vals), vals)
+defaultlimits(vals, colorrange) = colorrange isa NTuple{2} ? colorrange : defaultlimits(elscitype(vals), vals)
 defaultlimits(::Type, vals) = asfloat.(extrema(skipinvalid(vals)))
 defaultlimits(::Type{Distributional}, vals) = extrema(location.(skipinvalid(vals)))
-defaultlimits(vals::CategArray) = (0.0, asfloat(length(levels(vals))))
+defaultlimits(vals::CategArray, colorrange) = (0.0, asfloat(length(levels(vals))))
 
-defaultticks(vals) = range(defaultlimits(vals)..., 5)
-defaultticks(vals::CategArray) = 0:length(levels(vals))
+defaultticks(vals, limits) = range(limits..., 5)
+defaultticks(vals::CategArray, limits) = 0:length(levels(vals))
 
 defaultformat(vals::CategArray) = ticks -> map(t -> tick2level(t, levels(vals)), ticks)
 function defaultformat(vals)
