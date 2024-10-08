1c775518aa0415fc583768f004499d4956eec728
Elias Carvalho <73039601+eliascarv@users.noreply.github.com>
2024-10-08 10:17:38 -0300

    Fix `lenunit` option to handle coordinates with units (#137)
    * Fix 'lenunit' option to handle coordinates with units
    
    * Apply suggestions
    
    * Update implementation
    
    * Update src/georef.jl
    
    * Update comments
    
    ---------
    
    Co-authored-by: Júlio Hoffimann <julio.hoffimann@gmail.com>

--- test/georef.jl
+++ test/georef.jl
@@ -143,4 +143,17 @@
   gtb = georef(table, ("x", "y", "z"), crs=Spherical, lenunit=u"km")
   @test crs(gtb.geometry) <: Spherical
   @test unit(Meshes.lentype(gtb.geometry)) == u"km"
+  # retain the original units if `lenunit=nothing`
+  table = (a=rand(10), x=rand(10) * u"cm", y=rand(10) * u"cm")
+  gtb = georef(table, ("x", "y"))
+  @test crs(gtb.geometry) <: Cartesian
+  @test unit(Meshes.lentype(gtb.geometry)) == u"cm"
+  # convert the units if `lenunit` is passed
+  table = (a=rand(10), x=rand(10) * u"km", y=rand(10) * u"km")
+  gtb = georef(table, ("x", "y"), lenunit=u"m")
+  @test crs(gtb.geometry) <: Cartesian
+  @test unit(Meshes.lentype(gtb.geometry)) == u"m"
+  # error: the length unit of a `LatLon` CRS cannot be changed
+  table = (a=rand(10), lat=rand(10), lon=rand(10))
+  @test_throws ArgumentError georef(table, ("lat", "lon"), crs=LatLon, lenunit=u"km")
 end
