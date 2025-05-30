8ecef07256bbaa3b96e040d170b9ab5716b84f5d
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-05-30 11:49:47 -0300

    Fix docstring of asarray

--- src/api/geotables.jl
+++ src/api/geotables.jl
@@ -96,7 +96,7 @@ end
 """
     asarray(geotable, var)
 
-Returns the geotable for the variable `var` in `geotable` as a Julia array
+Returns the values of the variable `var` in `geotable` as a Julia array
 with size equal to the size of the underlying domain if the size is
 defined, otherwise returns a vector.
 """
