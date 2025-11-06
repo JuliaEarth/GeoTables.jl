9832a65883d862f76a9e3822911f1815e4b736c7
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-11-06 09:05:29 -0300

    Register error hint for Makie extension (#147)

--- src/GeoTables.jl
+++ src/GeoTables.jl
@@ -65,6 +65,34 @@ include("geotransforms.jl")
 # visualization
 include("viewer.jl")
 
+function __init__()
+  # register error hint for visualization functions
+  # since this is a recurring issue for new users
+  Base.Experimental.register_error_hint(MethodError) do io, exc, argtypes, kwargs
+    if exc.f == viewer || exc.f == cbar
+      if isnothing(Base.get_extension(GeoTables, :GeoTablesMakieExt))
+        print(
+          io,
+          """
+
+          Did you import a Makie.jl backend (e.g., GLMakie.jl, CairoMakie.jl) for visualization?
+
+          """
+        )
+        printstyled(
+          io,
+          """
+          julia> using GeoTables
+          julia> import GLMakie # or CairoMakie, WGLMakie, etc.
+          """,
+          color=:cyan,
+          bold=true
+        )
+      end
+    end
+  end
+end
+
 export
   # interface
   AbstractGeoTable,
